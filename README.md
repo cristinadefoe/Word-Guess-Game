513bcfcddf47935bb875cb04166ded4a8078bb0c
Cristina Defoe <cristina_defoe@yahoo.com>
2018-05-06 18:03:54 -0600

    Adding latest files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Word-Guess-Game
+Project Name:
+Word-Guess-Game 
+
+Description: 
+This is a simple game that allows the user to guess the word using the theme travel.
+
+How to Play the Game:
+-Press any key to start the game.
+-Select any letters from the keyboard. 
+-If you guess the letter correctly, the letter will be displayed under "Word to Guess". If you made a wrong guess, the letter will appear under "Wrong Guesses". The user is allowed up to 9 wrong guesses.
+-If you guess the word correctly you win the game. Otherwise, you lose the game and it will start over again.
+
+Built With:
+HTML
+CSS
+Javascript
+Jquery
+Bootstrap
+
+Author:
+Cristina Defoe
+
+Acknowledgments:
+Thanks to anyone's code that was used as inspiration.
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,119 @@
+// GLOBAL VARIABLES
+//*******************************************************************************************/
+
+//Arrays and variables for holding data
+var wordList = ["luggage", "colorado", "bahamas", "disneyland", "airplane", "camera",
+    "airport", "tourist", "paris", "jamaica"];
+var wordToGuess = "";
+var lettersGuessed = [];
+var numberGuessesRemaining = 0; //number of letters not guessed
+var emptyAndFilled = []; //e_ _ _ f _ _ _
+var incorrectLetters = [];
+
+//Game counters
+var winsCount = 0;
+var lossesCount = 0;
+var guessesLeft = 9;
+
+// Functions 
+//*******************************************************************************************/
+
+//Function start game
+function startGame() {
+    //Picks random word from wordList
+    wordToGuess = wordList[Math.floor(Math.random() * wordList.length)];
+    //Splits letters from wordList
+    lettersGuessed = wordToGuess.split("");
+    //Identifies how many letters there are in the lettersGuessed
+    numberGuessesRemaining = lettersGuessed.length;
+
+    //Reset
+    guessesLeft = 9;
+    incorrectLetters = [];
+    emptyAndFilled = [];
+
+    //Generate blanks and filled
+    for (var i = 0; i < numberGuessesRemaining; i++) {
+        emptyAndFilled.push("_");
+    }
+
+    document.getElementById("wordToGuess1").innerHTML = emptyAndFilled.join(" ");
+    document.getElementById("numberofGuessesRemaining1").innerHTML = guessesLeft;
+    document.getElementById("wins1").innerHTML = winsCount;
+    document.getElementById("losses1").innerHTML = lossesCount;
+
+    console.log(wordToGuess);
+    console.log(lettersGuessed);
+    console.log(numberGuessesRemaining);
+    console.log(emptyAndFilled);
+}
+
+function checkLetters(letter) {
+    //Check if letter exists in code
+    var lettersGuessed = false;
+
+    for (var i = 0; i < numberGuessesRemaining; i++) {
+        if (wordToGuess[i] == letter) {
+            lettersGuessed = true;
+        }
+    }
+
+    //Check the position in word letter exists and generate emptyAndFilled
+    if (lettersGuessed) {
+        for (var i = 0; i < numberGuessesRemaining; i++) {
+            if (wordToGuess[i] == letter) {
+                emptyAndFilled[i] = letter;
+            }
+        }
+    }
+    //Letter does not exist
+    else {
+        incorrectLetters.push(letter);
+        guessesLeft--;
+    }
+
+    console.log(emptyAndFilled);
+}
+
+function roundDone() {
+    console.log("Wins: " + winsCount + " | Losses: " + lossesCount + " | Guesses Left " + guessesLeft);
+
+    document.getElementById("numberofGuessesRemaining1").innerHTML = guessesLeft;
+    document.getElementById("wordToGuess1").innerHTML = emptyAndFilled.join(" ");
+    document.getElementById("wrongGuesses1").innerHTML = incorrectLetters.join(" ");
+
+    //Check if user won
+    if (lettersGuessed.toString() == emptyAndFilled.toString()) {
+        winsCount++
+        alert("You won!");
+
+        //Update wins in HTML
+        document.getElementById("wins1").innerHTML = winsCount;
+
+        startGame();
+    }
+
+    //Check if user lost
+    else if (guessesLeft == 0) {
+        lossesCount++;
+        alert("You lost!")
+
+        //Update loss in HTML
+        document.getElementById("losses1").innerHTML = lossesCount;
+
+        startGame();
+    }
+}
+
+// Main 
+//*******************************************************************************************/
+startGame();
+
+//Listen for letters entered by user
+document.onkeyup = function (event) {
+    var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(lettersGuessed);
+    roundDone();
+
+    console.log(lettersGuessed);
+}
